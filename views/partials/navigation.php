c7cc6c25604b53913311c817991af20b8a6c621e
DeAlbidar <albidarebenezernarh@gmail.com>
2023-09-14 09:30:39 +0000

    Navigation

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -55,14 +55,14 @@
                 <span class="title">find with me</span>
                 <ul class="social-share d-flex liststyle">
                     <li class="facebook">
-                        <a href="<?php echo URL.'index#'; ?>">
+                        <a href="https://facebook.com/DeAlbidar">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook">
                             <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                             </svg>
                         </a>
                     </li>
                     <li class="instagram">
-                        <a href="<?php echo URL.'index#'; ?>">
+                        <a href="https://www.instagram.com/DeAlbidar/">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
                             <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
@@ -71,12 +71,21 @@
                         </a>
                     </li>
                     <li class="linkedin">
-                        <a href="<?php echo URL.'index#'; ?>">
+                        <a href="https://www.linkedin.com/in/DeAlbidar">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
                             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z">
                             </path>
                             <rect x="2" y="9" width="4" height="12"></rect>
                             <circle cx="4" cy="4" r="2"></circle>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="twitter">
+                        <a href="https://twitter.com/DeAlbidar">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+                                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+                                <rect x="2" y="9" width="4" height="12"></rect>
+                                <circle cx="4" cy="4" r="2"></circle>
                             </svg>
                         </a>
                     </li>
@@ -113,7 +122,7 @@
     <div class="inner">
         <div class="menu-top">
             <div class="menu-header">
-                <a class="logo" href="index.html">
+                <a class="logo" href="<?php echo URL."index"; ?>">
                     <img src="<?php echo URL.'public/assets/images/logo/logos-circle.png'; ?>" alt="Personal Portfolio">
                 </a>
                 <div class="close-button">
@@ -154,14 +163,14 @@
                 <span class="title">find with me</span>
                 <ul class="social-share d-flex liststyle">
                     <li class="facebook">
-                        <a href="<?php echo URL.'index#'; ?>">
+                        <a href="https://facebook.com/DeAlbidar">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook">
                             <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                             </svg>
                         </a>
                     </li>
                     <li class="instagram">
-                        <a href="<?php echo URL.'index#'; ?>">
+                        <a href="https://www.instagram.com/DeAlbidar/">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
                             <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
@@ -170,11 +179,21 @@
                         </a>
                     </li>
                     <li class="linkedin">
-                        <a href="<?php echo URL.'index#'; ?>">
+                        <a href="https://www.linkedin.com/in/DeAlbidar">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
                             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z">
                             </path>
                             <rect x="2" y="9" width="4" height="12"></rect>
+                            <circle cx="4" cy="4" r="2"></circle>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="twitter">
+                        <a href="https://twitter.com/DeAlbidar">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+                            </path>
+                            <rect x="2" y="9" width="4" height="17"></rect>
                             <circle cx="4" cy="4" r="2"></circle>
                             </svg>
                         </a>
